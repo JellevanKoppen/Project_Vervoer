8feb88b1a80e80a29c0fa3acb646f4199550d75c
0944862 <0944862@hr.nl>
2017-11-21 12:38:45 +0100

    Nieuwe functies

--- DBController.php
+++ DBController.php
@@ -1,25 +1,14 @@
 <?php
-echo This works!
+
 session_start();
 
-// Hier kan je een tekst opgeven die je tabellen vooraf gaat.
-// Hierdoor kan je meerdere loginsystemen gebruiken in 1 database,
-// zonder de code te moeten aanpassen. Bijvoorbeeld MD_
-$db_prefix = "";
-// Je host, meestal localhost.
-$mysql_host = "localhost";
-// Gebruikersnaam om toegang te krijgen tot je database
-$mysql_user = "root";
-// ... en bijhorend wachtwoord
-$mysql_pwd = "Password";
-// last but not least: je database
-$mysql_db = "Vervoer_Project";
-
-// MySQL connectie maken
+$mysql_host = "http://www.dewillem.nu/phpmyadmin";
+$mysql_user = "v6vervoer04";
+$mysql_pwd = "v6vervoer04&";
+$mysql_db = "v6vervoer04";
 
 $conn = new mysqli($mysql_host, $mysql_user, $mysql_pwd, $mysql_db);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
